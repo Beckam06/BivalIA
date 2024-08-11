8b39fe6e9647a43becfe490923a1bc124f104a9c
onielos <onielosespinal@gmail.com>
2024-08-10 18:40:42 -0600

    .

--- bivalbos/urls.py
+++ bivalbos/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio, name='inicio'),
+    path("test", views.test, name="test"),
+    path("formas", views.formas, name='formas' )
 ]
--- bivalbos/views.py
+++ bivalbos/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse('Hola mundo')
+    
+def test(request):
+    return render(request,'paginas/test.html')
+def formas(request):
+    return render(request,'formas/index.html')
 # Create your views here.
